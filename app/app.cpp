b8539644776d76e71b9ae332105eb8038345678f
Ivan Selendic <ivan.selendic@gmail.com>
2022-11-15 17:23:03 +0100

    app.h,app.c

--- app/app.cpp
+++ app/app.cpp
@@ -3,5 +3,15 @@
 
 namespace vsite::oop::v5
 {
+	double operator "" _inch(unsigned long long n) {
+		return 2.54 * n;
+	}
+	double operator "" _eur(unsigned long long n) {
+		return 7.543 * n;
+	}
 
+	double diameter() {};
+	int year() {};
+	double price{};
+	
 }
--- app/app.h
+++ app/app.h
@@ -4,5 +4,35 @@
 
 namespace vsite::oop::v5 
 {
+	double operator "" _inch(unsigned long long x);
 
+	double operator "" _eur(unsigned long long n);
+
+	class tire {
+		double width;
+	public: 
+		tire(double const& width) :width(width){}
+		double	diameter() {
+	}
+	};
+
+	class vehicle {
+		int year_of_production;
+	public: 
+		
+		vehicle(int const& y) : year_of_production(y){}
+				int year() {
+		}
+	};
+
+	class product {
+		double product_price;
+	public:
+		product(double const& price):product_price(price){}
+		double price(){}
+	};
+
+	class car:public product,public vehicle,public tire {
+
+	};
 }
