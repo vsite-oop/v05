b306dc756ee353505440a6af8e8925397b5d89d2
Matko <tkoma999@gmail.com>
2022-11-16 18:28:17 +0100

    v05

--- app/app.cpp
+++ app/app.cpp
@@ -4,4 +4,41 @@
 namespace vsite::oop::v5
 {
 
+    double operator"" _eur(unsigned long long int n)
+    {
+        return n * 7.5345;
+    }
+    double operator"" _inch(unsigned long long int n)
+    {
+        return n * 2.54;
+    }
+    vehicle::vehicle(unsigned int a) : y{a} {}
+
+    unsigned int vehicle::year() const
+    {
+        return y;
+    }
+
+    tire::tire(double d) : t{d} {}
+
+    double tire::diameter() const
+    {
+        return t;
+    }
+
+    product::product(double pr) : p{pr} {}
+
+    double product::price() const
+    {
+        return p;
+    }
+    
+    car::car(unsigned int a,double pr,double tdi):vehicle(a),product(pr),td{tdi}{}
+
+    double car::tire_diameter()const{
+        return td;
+    }     
+         
+
+
 }
--- app/app.h
+++ app/app.h
@@ -4,5 +4,39 @@
 
 namespace vsite::oop::v5 
 {
+    double operator "" _eur(unsigned long long int n);
+    
+    double operator "" _inch(unsigned long long int n);
+
+    class tire{
+        double t;
+        public:
+         tire::tire(double d);
+         double diameter()const;
+
+        
+    };
+    class vehicle{
+        unsigned int y;
+        public:
+         vehicle::vehicle(unsigned int a);
+         unsigned int year()const;
+
+    };
+    class product{
+        double p;
+        public:
+         product::product(double pr);
+         double price()const;
+
+    };
+    
+    class car:public vehicle,public product{
+        double td;
+        public:
+         car::car(unsigned int a,double pr,double tdi);
+         double tire_diameter()const;
+
+    };
 
 }
