e42e816023be70e37b5fa6df2aee9e15250b6878
louis20th <lujomirs@gmail.com>
2023-01-29 17:09:30 +0100

    Task done

--- app/app.cpp
+++ app/app.cpp
@@ -3,5 +3,49 @@
 
 namespace vsite::oop::v5
 {
+    double operator "" _inch(unsigned long long n) {
+        return n * 2.54;
+    }
 
+    double operator "" _eur(unsigned long long m) {
+        return m * 7.5345;
+    }
+
+    tire::tire(const double& dia) 
+        : size{ dia }
+    {
+    }
+
+    double tire::diameter() const {
+        return size;
+    }
+
+    vehicle::vehicle(const int& year)
+        : builtDate{ year }
+    {
+    }
+
+    int vehicle::year() const {
+        return builtDate;
+    }
+
+    product::product(const double& value) 
+        : valuedAt{ value }
+    {
+    }
+
+    double product::price() const {
+        return valuedAt;
+    }
+
+    car::car(const int& y, const double& p, const double& t) 
+        : vehicle{ y }
+        , product{ p }
+        , tires{ tire(t) }
+    {
+    }
+
+    double car::tire_diameter() const {
+        return tires.diameter();
+    }
 }
--- app/app.h
+++ app/app.h
@@ -4,5 +4,38 @@
 
 namespace vsite::oop::v5 
 {
+    double operator "" _inch(unsigned long long n);
+    double operator "" _eur(unsigned long long m);
 
+    class tire {
+        double size;
+    public:
+        tire(const double& size);
+        ~tire() = default;
+        double diameter() const;
+    };
+
+    class vehicle {
+        int builtDate;
+    public:
+        vehicle(const int& year);
+        ~vehicle() = default;
+        int year() const;
+    };
+
+    class product {
+        double valuedAt;
+    public:
+        product(const double& value);
+        ~product() = default;
+        double price() const;
+    };
+
+    class car : public vehicle, public product {
+        tire tires;
+    public:
+        car(const int& y, const double& p, const double& t);
+        ~car() = default;
+        double tire_diameter() const;
+    };
 }
