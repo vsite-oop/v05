6bc664a34ddcbb49586848bbac0688633d765a4f
DrHrast <petar.huljek@gmail.com>
2023-11-13 20:58:11 +0100

    P_huljek
    vježba 5, bez greške.

--- app/app.h
+++ app/app.h
@@ -4,5 +4,35 @@
 
 namespace vsite::oop::v5 
 {
+	double operator "" _inch(unsigned long long length);
 
+	double operator "" _eur(unsigned long long money);
+
+	class vehicle {
+		int yr;
+	public:
+		vehicle(int yr);
+		int year();
+	};
+
+	class product {
+		double pr;
+	public:
+		product(double pr);
+		double price();
+	};
+
+	class tire {
+		double size_of_tire;
+	public:
+		tire(double size_of_tire);
+		double diameter();
+	};
+
+	class car : public vehicle, public product {
+		tire cars_tire;
+	public:
+		car(int yr, double pr, double size_of_tire);
+		double tire_diameter();
+	};
 }
