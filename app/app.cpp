e0709fe710e183499a3b2555a2b3dada82025a92
Bogjenero <bornagjenero1@gmail.com>
2023-11-13 20:06:27 +0100

    Rijeseni  zadaci

--- app/app.cpp
+++ app/app.cpp
@@ -3,5 +3,28 @@
 
 namespace vsite::oop::v5
 {
-
+	double tire::diameter()
+	{
+		return b ;
+	}
+	int vehicle::year()
+	{
+		return godina;
+	}
+	double operator ""_eur(unsigned long long int broj)
+	{
+		return broj * 7.5345;
+	}
+	double operator ""_inch(unsigned long long int  broj)
+	{
+		return broj * 2.54;
+	}
+	double product::price()
+	{
+		return cijena ;
+	}
+	double car::tire_diameter()
+	{
+		return t.diameter() ;
+	}
 }
--- app/app.h
+++ app/app.h
@@ -4,5 +4,32 @@
 
 namespace vsite::oop::v5 
 {
+	double operator "" _inch(unsigned long long int broj);
+	class tire {
+		double b;
+	public:
+		 tire(double broj) : b(broj) {}
+		 double diameter();
+	};
+	class vehicle
+	{
+		int godina;
+	public:
+		vehicle(int g) : godina(g) {}
+		int year();
+	};
+	double operator ""_eur(unsigned long long int broj);
+	class product {
+		double cijena;
+	public:
+		product(double d) : cijena(d) {};
+		double price();
+	};
+	class car : public vehicle ,public product{
+		tire t;
+	public:
+		car(int g, double c, double i) : vehicle(g), product(c), t(i) {}
+		double tire_diameter();
 
+	};
 }
