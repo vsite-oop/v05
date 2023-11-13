c7dc63093c44b3c2bc0c142887c495a0458f563d
Sonja Perković <soperkov@vsite.hr>
2023-11-13 20:57:36 +0100

    sperkovic Rijesena vjezba 5

--- app/app.cpp
+++ app/app.cpp
@@ -3,5 +3,40 @@
 
 namespace vsite::oop::v5
 {
+	double operator "" _inch(unsigned long long d) {
+		return d * 2.54;
+	}
+	double operator "" _eur(unsigned long long d) {
+		return d * 7.5345;
+	}
 
+	vehicle::vehicle(int year) {
+		productionYear = year;
+	}
+
+	int vehicle::year (){
+		return productionYear;
+	}
+
+	tire::tire(double sot) {
+		sizeOfTire = sot;
+	}
+
+	double tire::diameter() {
+		return sizeOfTire;
+	}
+
+	product::product(double price) {
+		productPrice = price;
+	}
+
+	double product::price() {
+		return productPrice;
+	}
+	
+	car::car(int yearofproduction, double carprice, double sizeoftire) : vehicle(yearofproduction), product(carprice), newTire(sizeoftire){}
+
+	double car::tire_diameter() {
+		return newTire.diameter();
+	}
 }
--- app/app.h
+++ app/app.h
@@ -4,5 +4,35 @@
 
 namespace vsite::oop::v5 
 {
+	double operator "" _inch(unsigned long long d);
+	double operator "" _eur(unsigned long long d);
 
+	class vehicle {
+		int productionYear;
+	public:
+		vehicle(int yr);
+		int year();
+	};
+
+	class tire {
+		double sizeOfTire;
+	public:
+		tire(double size_of_tire);
+		double diameter();
+	};
+
+
+	class product {
+		double productPrice;
+	public:
+		product(double p);
+		double price();
+	};
+
+	class car : public vehicle, public product {
+		tire newTire;
+	public:
+		car(int yearofproduction, double carprice, double sizeoftire);
+		double tire_diameter();
+	};
 }
