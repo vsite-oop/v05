ffc533bc36fd0d7eda982663cc5499ed706199d8
AnMal <junkie_@msn.com>
2023-11-14 07:00:57 +0100

    Antonio Maletic

--- app/app.cpp
+++ app/app.cpp
@@ -3,5 +3,27 @@
 
 namespace vsite::oop::v5
 {
+    double operator"" _inch(unsigned long long n) {
+        return n * 2.54;
+    }
 
+    double operator"" _eur(unsigned long long n) {
+        return n * 7.5345;
+    }
+
+    double tire::diameter() {
+        return d;
+    }
+
+    int vehicle::year() {
+        return y;
+    }
+
+    double product::price() {
+        return p;
+    }
+
+    double car::tire_diameter() {
+        return t.diameter();
+    }
 }
