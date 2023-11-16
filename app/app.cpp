facc4a87318653108ba5ea32eb1d2abf95f98f2c
Mihael Krpes <mikrpes@vsite.hr>
2023-11-16 15:50:39 +0100

    v05

--- app/app.cpp
+++ app/app.cpp
@@ -3,5 +3,37 @@
 
 namespace vsite::oop::v5
 {
+	double operator "" _inch(unsigned long long x) {
+		return x * 2.54;
+	}
+	double operator "" _eur(unsigned long long kn) {
+		return kn * 7.5345;
+	}
 
+	tire::tire(double dm) {
+		dia = dm;
+	}
+	double tire::diameter() const{
+		return dia;
+	}
+
+	vehicle::vehicle(const int y) {
+		YoM = y;
+	}
+	int vehicle::year() const{
+		return YoM;
+	}
+
+	product::product(double val) {
+		value = val;
+	}
+	double product::price() const{
+		return value;
+	}
+
+	car::car(const int y, double val, double dm) : vehicle(y), product(val), tyre(dm) {}
+	
+	double car::tire_diameter() const{
+		return tyre.diameter();
+	}
 }
--- app/app.h
+++ app/app.h
@@ -4,5 +4,33 @@
 
 namespace vsite::oop::v5 
 {
+	double operator "" _inch(unsigned long long x);
+	double operator "" _eur(unsigned long long kn);
 
+	class tire {
+		double dia;
+	public:
+		tire(double dia);
+		double diameter() const;
+	};
+	class vehicle {
+		int YoM;
+	public:
+		vehicle(const int y);
+		int year() const;
+	};
+	class product {
+		double value;
+	public:
+		product(double value);
+		double price() const;
+
+	};
+	class car: public vehicle, public product {
+		tire tyre;
+
+	public:
+		car(const int y, double value, double dia);
+		double tire_diameter() const;
+	};
 }
