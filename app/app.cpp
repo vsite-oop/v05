b5dfa3c5cb05e98e42d218b48447c824231cdf0e
Josip Grabušić <jograbus@vsite.hr>
2023-11-26 17:45:31 +0100

    Vježba V05

--- app/app.cpp
+++ app/app.cpp
@@ -3,5 +3,33 @@
 
 namespace vsite::oop::v5
 {
+    double operator"" _inch(unsigned long long x) 
+    {
+        return x * 2.54;
+    }
 
+    double operator"" _eur(unsigned long long x) 
+    {
+        return x * 7.5345;
+    }
+
+    int vehicle::year() 
+    {
+        return yr;
+    }
+
+    double tire::diameter() 
+    {
+        return dia;
+    }
+
+    double product::price() 
+    {
+        return pr;
+    }
+
+    double car::tire_diameter() 
+    {
+        return t.diameter();
+    }
 }
--- app/app.h
+++ app/app.h
@@ -2,7 +2,46 @@
 #include <string>
 #include <memory>
 
-namespace vsite::oop::v5 
+namespace vsite::oop::v5
 {
+    double operator"" _inch(unsigned long long x);
+    double operator"" _eur(unsigned long long x);
 
+    class product 
+    {
+        double pr;
+    public:
+        product(double x) : pr(x) {};
+
+        double price();
+    };
+
+
+    class vehicle 
+    {
+        int yr;
+    public:
+        vehicle(int x) : yr(x) {};
+
+        int year();
+    };
+
+    class tire 
+    {
+        double dia;
+    public:
+        tire(double x) : dia(x) {};
+
+        double diameter();
+    };
+
+
+    class car : public product, public vehicle 
+    {
+        tire t;
+    public:
+        car(int yr, double pr, double dia) : vehicle(yr), product(pr), t(dia) {};
+
+        double tire_diameter();
+    };
 }
