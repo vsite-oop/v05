9a48ab6e989451b5cbf656e0b99465788a318991
Đuro Rukavina <djrukavina@vsite.hr>
2023-11-29 19:16:59 +0100

    v05 solved

--- app/app.cpp
+++ app/app.cpp
@@ -3,5 +3,38 @@
 
 namespace vsite::oop::v5
 {
+	vehicle::vehicle(int year_of_manufacture) {
+		this->yom = year_of_manufacture;
+	}
+	int vehicle::year() {
+		return this->yom;
+	}
 
+	tire::tire(double tire_diameter) {
+		this->dia = tire_diameter;
+	}
+	double tire::diameter() {
+		return this->dia;
+	}
+
+	product::product(double product_price) {
+		this->pr = product_price;
+	}
+	double product::price() {
+		return this->pr;
+	}
+
+	car::car(int year_of_manufacture, double price, double tire_diameter) : 
+	vehicle(year_of_manufacture), product(price), t(tire_diameter){}
+
+	double car::tire_diameter() {
+		return t.diameter();
+	}
+	
+	double operator ""_inch(unsigned long long in) {
+		return in * 2.54;
+	}
+	double operator ""_eur(unsigned long long eu) {
+		return eu * 7.5345;
+	}
 }
--- app/app.h
+++ app/app.h
@@ -4,5 +4,38 @@
 
 namespace vsite::oop::v5 
 {
+	class vehicle {
+		int yom;
+	public:
+		vehicle(int year_of_manufacture);
+		int year();
+		~vehicle() {}
+	};
 
+	class tire {
+		double dia;
+	public:
+		tire(double tire_diameter);
+		double diameter();
+		~tire() {}
+	};
+
+	class product {
+		double pr;
+	public:
+		product(double product_price);
+		double price();
+		~product() {}
+	};
+
+	class car : public vehicle, public product {
+		tire t;
+	public:
+		car(int year_of_manufacture, double price, double tire_diameter);
+		double tire_diameter();
+		~car() {}
+	};
+
+	double operator ""_inch(unsigned long long in);
+	double operator ""_eur(unsigned long long eu);
 }
