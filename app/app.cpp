690014ad45a0c886d43a579a309c4bffb5e96e01
Marko Pavić <mapavic@vsite.hr>
2023-11-30 17:19:09 +0100

    Marko Pavić

--- app/app.cpp
+++ app/app.cpp
@@ -3,5 +3,26 @@
 
 namespace vsite::oop::v5
 {
+    double operator"" _inch(unsigned long long num){
+        return num * 2.54;
+    }
+    double operator"" _eur(unsigned long long eu){
+        return eu * 7.5345;
+    }
 
+    double tire::diameter(){
+        return d;
+    }
+
+    int vehicle::year(){
+        return yr;
+    }
+
+    double product::price(){
+        return pr;
+    }
+
+    double car::tire_diameter(){
+        return t.diameter();
+    }
 }
--- app/app.h
+++ app/app.h
@@ -4,5 +4,38 @@
 
 namespace vsite::oop::v5 
 {
+    double operator"" _inch(unsigned long long num);
+    double operator"" _eur(unsigned long long eu);
 
+    class tire {
+        double d;
+    public:
+        tire(double value) : d(value){};
+        double diameter();
+    };
+
+    class vehicle {
+        int yr;
+    public:
+        vehicle(int value) : yr(value){};
+        int year();
+    };
+
+   
+
+    class product {
+        double pr;
+    public:
+        product(double value) : pr(value){};
+        double price();
+    };
+
+
+    class car : public vehicle, public product {
+        tire t;
+    public:
+        car(int yr, double pr, double d) : vehicle(yr), product(pr), t(d){};
+
+        double tire_diameter();
+    };
 }
