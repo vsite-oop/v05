519528895fa72c495d89f7a73b65f9c631eb9df7
Hrvoje Janđel <hrjandje@vsite.hr>
2023-12-01 18:29:32 +0100

    vj 5 resolved

--- app/app.cpp
+++ app/app.cpp
@@ -3,5 +3,28 @@
 
 namespace vsite::oop::v5
 {
+   
+    double operator"" _eur(unsigned long long e) {
+        return e * 7.5345;
+    }
+    
+    double operator"" _inch(unsigned long long in) {
+        return in * 2.54;
+    }
+    
+    double tire::diameter() {
+        return d;
+    }
 
+    int vehicle::year() {
+        return y;
+    }
+
+    double product::price() {
+        return p;
+    }
+
+    double car::tire_diameter() {
+        return t.diameter();
+    }
 }
--- app/app.h
+++ app/app.h
@@ -2,7 +2,43 @@
 #include <string>
 #include <memory>
 
+
 namespace vsite::oop::v5 
 {
+    double operator"" _inch(unsigned long long in);
 
+    double operator"" _eur(unsigned long long e);
+
+    class tire {
+        double d;
+    public:
+        tire(double e) : d(e) {};
+
+        double diameter();
+    };
+
+    class vehicle {
+        int y;
+    public:
+        vehicle(int e) : y(e) {};
+
+        int year();
+    };
+
+    class product {
+        double p;
+    public:
+        product(double e) : p(e) {};
+
+        double price();
+    };
+
+
+    class car : public vehicle, public product {
+        tire t;
+    public:
+        car(int y, double p, double d) : vehicle(y), product(p), t(d) {};
+
+        double tire_diameter();
+    };
 }
