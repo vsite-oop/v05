3b7a7231759e0fb9086f873f71f0b8d7b198931f
Tin Bašić <tin.basic22@gmail.com>
2023-12-03 21:43:37 +0100

    Tin Bašić

--- app/app.h
+++ app/app.h
@@ -2,7 +2,40 @@
 #include <string>
 #include <memory>
 
-namespace vsite::oop::v5 
+namespace vsite::oop::v5
 {
+    double operator"" _inch(unsigned long long n);
 
+    class tire {
+        double d;
+    public:
+        tire(double n) : d(n) {};
+
+        double diameter();
+    };
+
+    class vehicle {
+        int y;
+    public:
+        vehicle(int n) : y(n) {};
+
+        int year();
+    };
+
+    class product {
+        double p;
+    public:
+        product(double n) : p(n) {};
+
+        double price();
+    };
+
+
+    class car : public vehicle, public product {
+        tire t;
+    public:
+        car(int y, double p, double d) : vehicle(y), product(p), t(d) {};
+
+        double tire_diameter();
+    };
 }
